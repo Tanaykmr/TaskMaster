3a846aacb2b58444508850741632591f25f9f794
Tanay Kumar <111867105+Tanaykmr@users.noreply.github.com>
2024-02-09 14:50:12 +0530

    creatd signin page, divided signup.tsx into components for modularity

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,5 +1,5 @@
-// import {useNavigate} from 'react-router-dom';
-import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 import axios from "axios";
 import { Button, Typography } from "@mui/material";
 import TextField from '@mui/material/TextField';
@@ -17,6 +17,39 @@ if (!BASE_URL) {
 
 // TODO: add box insflatead of div to remove br and make it cleaner, remove id custom names
 const Signup = () => {
+
+	return (
+		<div id="testing-div" style={{ display: "flex", justifyContent: "space-between", marginTop: "40px" }}>
+			<Welcome />
+			<SignupCard />
+		</div>
+	);
+
+}
+
+function Welcome() {
+	return (
+		<div id="welcome" style={{ marginLeft: "80px", display: "flex", alignItems: "center", flexDirection: "column", height: "100vh" }}>
+			<Typography variant="h4" style={{ color: "white", marginTop: "50px", }}>Welcome to</Typography>
+			<img src="https://cdn-icons-png.freepik.com/512/3585/3585145.png?ga=GA1.1.1023148225.1707385329&" style={{ width: "100px", paddingTop: "20px" }}></img>
+			<Typography variant="h2" style={{ color: "white", marginTop: "20px", fontWeight: "500" }}>TaskMaster</Typography>
+
+			<Typography variant="subtitle1" style={{ color: "white", marginTop: "50px", textAlign: "center" }}>Your trusted ally in productivity, streamlining your to-dos with precision, <br />so you can focus on what matters most.</Typography>
+
+			<div style={{ display: "flex", justifyContent: "space-between", marginTop: "300px", marginBottom: "20px" }}>
+				<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" style={{ width: "50px", marginRight: "50px" }} onClick={() => {
+					window.location.href = "http://github.com/tanaykmr"
+				}} />
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Twitter_X.png/640px-Twitter_X.png" style={{ width: "69px" }} onClick={() => {
+					window.location.href = "http://twitter.com/tanaykmr"
+				}} />
+			</div>
+		</div>
+	)
+}
+
+function SignupCard() {
+	const navigate = useNavigate();
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	console.log("BASE_URL is: ", BASE_URL);
@@ -39,80 +72,59 @@ const Signup = () => {
 		});
 	}
 
-
 	return (
-		<div id="testing-div" style={{ display: "flex", justifyContent: "space-between", marginTop: "40px" }}>
-			<div id="welcome" style={{ marginLeft: "80px", display: "flex", alignItems: "center", flexDirection: "column", height: "100vh" }}>
-				<Typography variant="h4" style={{ color: "white", marginTop: "50px", }}>Welcome to</Typography>
-				<img src="https://cdn-icons-png.freepik.com/512/3585/3585145.png?ga=GA1.1.1023148225.1707385329&" style={{ width: "100px", paddingTop: "20px" }}></img>
-				<Typography variant="h2" style={{ color: "white", marginTop: "20px", fontWeight: "500" }}>TaskMaster</Typography>
+		<div id="create-acc" style={{ marginRight: "50px", display: "flex", flexDirection: "column" }}>
+			<Typography variant="h3" style={{ margin: "0 80px 60px", }}>Create your account</Typography>
+			<Typography variant="h6">Name</Typography>
+			<TextField
+				id="standard-required"
+				label="Enter your name"
+				variant="standard"
+				fullWidth={true}
+			/>
+			<Typography variant="h6" style={{ marginTop: "40px" }}>Email</Typography>
+			{/* TODO: add validation to check if valid email */}
+			<TextField
+				id="standard-required"
+				label="Enter your email address"
+				defaultValue="Hello World"
+				variant="standard"
+				fullWidth={true}
+				value={username}
+				onChange={(e) => {
+					setUsername(e.target.value)
+				}}
+			/>
+			<Typography variant="h6" style={{ marginTop: "40px" }}>Password</Typography>
+			{/* TODO: add password meter from mui to see if password is strong */}
+			<TextField
+				id="standard-password-input"
+				label="Password"
+				type="password"
+				autoComplete="current-password"
+				variant="standard"
+				fullWidth={true}
+				value={password}
+				onChange={(e) => {
+					setPassword(e.target.value)
+				}}
+			/>
 
-				<Typography variant="subtitle1" style={{ color: "white", marginTop: "50px", textAlign: "center" }}>Your trusted ally in productivity, streamlining your to-dos with precision, <br />so you can focus on what matters most.</Typography>
+			<div style={{ display: "flex", justifyContent: "flex-start", marginTop: "40px" }}>
+				<Button variant="contained" size="large" style={{ marginRight: "40px" }} onClick={() => {
+					console.log("inside signup button in signup.tsx, username is: ", username, "password is: ", password);
+					handleSignup();
+				}}>
+					Sign up
+				</Button>
 
-				<div style={{ display: "flex", justifyContent: "space-between", marginTop: "300px", marginBottom: "20px" }}>
-					<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" style={{ width: "50px", marginRight: "50px" }} onClick={() => {
-						window.location.href = "http://github.com/tanaykmr"
-					}} />
-					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Twitter_X.png/640px-Twitter_X.png" style={{ width: "69px" }} onClick={() => {
-						window.location.href = "http://twitter.com/tanaykmr"
-					}} />
-				</div>
-			</div>
-
-			{/* TODO: make flex direction column? */}
-			<div id="create-acc" style={{ marginRight: "50px", display: "flex", flexDirection: "column" }}>
-				<Typography variant="h3" style={{ margin: "0 80px 60px", }}>Create your account</Typography>
-				<Typography variant="h6">Name</Typography>
-				<TextField
-					id="standard-required"
-					label="Enter your name"
-					variant="standard"
-					fullWidth={true}
-				/>
-				<Typography variant="h6" style={{ marginTop: "40px" }}>Email</Typography>
-				{/* TODO: add validation to check if valid email */}
-				<TextField
-					id="standard-required"
-					label="Enter your email address"
-					defaultValue="Hello World"
-					variant="standard"
-					fullWidth={true}
-					value={username}
-					onChange={(e) => {
-						setUsername(e.target.value)
-					}}
-				/>
-				<Typography variant="h6" style={{ marginTop: "40px" }}>Password</Typography>
-				{/* TODO: add password meter from mui to see if password is strong */}
-				<TextField
-					id="standard-password-input"
-					label="Password"
-					type="password"
-					autoComplete="current-password"
-					variant="standard"
-					fullWidth={true}
-					value={password}
-					onChange={(e) => {
-						setPassword(e.target.value)
-					}}
-				/>
-
-				<div style={{ display: "flex", justifyContent: "flex-start", marginTop: "40px" }}>
-					<Button variant="contained" size="large" style={{ marginRight: "40px" }} onClick={() => {
-						console.log("inside signup button in signup.tsx, username is: ", username, "password is: ", password);
-						handleSignup();
-					}}>
-						Sign up
-					</Button>
-
-					<Button variant="outlined" size="large">
-						Sign in
-					</Button>
-				</div>
+				<Button variant="outlined" size="large" onClick={() => {
+					navigate("/signin");
+				}}>
+					Sign in
+				</Button>
 			</div>
 		</div>
-	);
-
+	)
 }
-
 export default Signup;
