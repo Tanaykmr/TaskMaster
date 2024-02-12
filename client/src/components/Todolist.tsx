bd744063eb69bf2e00e8f0818c6dfca22e86ec01
Tanay Kumar <111867105+Tanaykmr@users.noreply.github.com>
2024-02-12 10:05:48 +0530

    made the add and delete buttons(functionality is pending), added glassmorphism, styled the entire todolist page

--- client/src/components/Todolist.tsx
+++ client/src/components/Todolist.tsx
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {Card, Fab, TextField, Typography} from "@mui/material";
+import Checkbox from '@mui/material/Checkbox';
 import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 if (!BASE_URL) {
@@ -9,43 +12,23 @@ if (!BASE_URL) {
     process.exit(1);
 }
 
+const label = {inputProps: {'aria-label': 'Checkbox demo'}};
 
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 
-// // Create a theme instance
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#ff5722', // Set the primary color
-//     },
-//     text: {
-//       secondary: '#f50057', // Set the secondary text color
-//     },
-//   },
-// });
+// Create a theme instance
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ba142d', // Set the primary color
+            // @ts-expect-error idk why using secondary as color is throwing an error
+            secondary: "#294B29",
 
-// const MyComponent = () => {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Box
-//         sx={{
-//           bgcolor: 'primary.main',
-//           color: 'text.secondary',
-//           border: 4,
-//           borderRadius: 2,
-//           px: 2,
-//           fontWeight: 'fontWeightBold',
-//           zIndex: 'tooltip',
-//           boxShadow: 8,
-//         }}
-//       >
-//         Box
-//       </Box>
-//     </ThemeProvider>
-//   );
-// };
-// export default MyComponent;
+        }, text: {
+            secondary: '#f50057', // Set the secondary text color
+        },
+    },
+});
 
 interface TodoStyleInterface {
     _id?: string,
@@ -56,20 +39,7 @@ interface TodoStyleInterface {
 
 const Todolist = () => {
     const [todos, setTodos] = useState<TodoStyleInterface[]>([]);
-    // const getTodos = async () => {
-    //     await axios.get(`${BASE_URL}/todos/all`, {
-    //         headers: {
-    //             Authorization: "Bearer " + localStorage.getItem("authorization")
-    //         }
-    //     }).then((response) => {
-    //         console.log("response is: ", response)
-    //         console.log("from response, todos are: ", response.data.todos);
-    //         setTodos(response.data.todos);
-    //     })
-    // }
-
     useEffect(() => {
-        // getTodos();
         axios.get(`${BASE_URL}/todos/all`, {
             headers: {
                 Authorization: "Bearer " + localStorage.getItem("authorization")
@@ -84,13 +54,30 @@ const Todolist = () => {
     console.log("Outside useEffect Todos are: ", todos);
 
     return (<div id="return-mother-div"
-                 style={{display: "flex", flexDirection: "column", alignItems: "center", background: "orange"}}>
-        <Card variant="outlined" sx={{borderRadius: "15px"}} style={{background: "#f0f0f0", marginBottom: "10px"}}>
-            <Typography variant="h2" style={{padding: "5px"}}>TaskMaster </Typography>
+                 style={{
+                     padding: "10px 20px",
+                     width: "fit-content",
+                     display: "flex",
+                     flexDirection: "column",
+                     alignItems: "center",
+                     background: "rgba(255, 255, 255, 0.25)", // Semi-transparent background
+                     backdropFilter: "blur(10px)", // Apply blur effect
+                     border: "1px solid rgba(255, 255, 255, 0.18)", // Border for contrast
+                     borderRadius: "10px", // Rounded corners
+                     boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)", // Box shadow for depth
+                     WebkitBackdropFilter: "blur(10px)", // Apply blur effect (for Safari)
+                 }}>
+        <Card variant="outlined" sx={{borderRadius: "15px"}} style={{background: "#FEDB39", marginBottom: "10px"}}>
+            <Typography variant="h2"
+                        style={{
+                            padding: "10px", fontFamily: 'Bungee Inline',
+                        }}>TaskMaster </Typography>
         </Card>
         <CreateTodoComponent setTodos={setTodos}/>
         {todos.map((todo) => {
-            return (<TodoListComponent key={todo._id} description={todo.description}/>)
+            return (<TodoListComponent key={todo._id} description={todo.description} id={todo._id} todos={todos}
+                                       setTodos={setTodos}
+            />)
         })}
     </div>)
 }
@@ -99,7 +86,7 @@ function CreateTodoComponent(props) {
     const [newTodo, setNewTodo] = useState([])
     return (<div id="tododiv" style={{display: "flex", justifyContent: "space-between", padding: "5px",}}>
         <TextField id="filled-basic" label="write your next task" variant="outlined" sx={{borderRadius: "15px",}}
-                   style={{background: "yellow", marginRight: "10px",}} onChange={((e) => {
+                   style={{marginRight: "10px", marginBottom: "25px"}} onChange={((e) => {
             console.log("e.target.value :", e.target.value);
             setNewTodo(e.target.value);
             console.log("newTodo: ", newTodo);
@@ -124,18 +111,30 @@ function CreateTodoComponent(props) {
 }
 
 function TodoListComponent(props) {
-    return (<Card style={{
-        background: "#f0f0f0",
-        width: "200px",
-        height: "40px",
-        marginBottom: "5px",
-        padding: "3px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    }}>
-        <Typography variant="subtitle1">{props.description}</Typography>
-    </Card>)
+    return (<div style={{display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "10px"}}>
+        <Checkbox {...label} color="success"/>
+        <Card style={{
+            background: "#6c57f2",
+            width: "200px",
+            height: "30px",
+            marginRight: "10px",
+            padding: "3px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <Typography variant="subtitle1" align="center"
+                        style={{color: "white"}}>{props.description}</Typography>
+        </Card>
+        <ThemeProvider theme={theme}>
+            <Fab
+                style={{height: "40px", width: "40px"}}>
+                <DeleteIcon sx={{color: 'primary.main',}} style={{height: "25px", width: "25px"}}/>
+            </Fab>
+        </ThemeProvider>
+
+    </div>);
 }
+
 
 export default Todolist;
